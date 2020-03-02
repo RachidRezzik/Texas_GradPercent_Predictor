36651d0729af1aa6d0150fce571c05c5dd1137b4
Rachid Rezzik <rachid.rezzik@hotmail.com>
2020-03-01 22:26:42 -0600

    started report and powerpoint

--- Texas_Forecaster.py
+++ Texas_Forecaster.py
@@ -20,47 +20,52 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 ### Fixing College Enrollment DFs ###
-# SAT = pd.read_csv('Total_SAT.csv')
-# SAT = SAT[['DistName', 'RegnName', 'Year']]
-# path = 'College_Enrollment/*'
-# enrol_files = glob.glob(path)
-# fixed_enrol_dfs = []
-# for file_name in enrol_files:
-#     file = pd.read_csv(file_name)
-#     file = file[['DistName', 'Students Enrolled in Texas Public 4-year Universities', 'Total High School Graduates']]
-#     file.columns = ['DistName', 'Enrolled 4-Year', 'Total Graduated']
-#     col_list = file.columns[1:]
-#     dist = list(file['DistName'])
-#     dist = [str(i) for i in dist]
-#     dist = [i[9:] for i in dist]
-#     file['DistName'] = pd.Series(dist)
-#     for col in col_list:
-#         new_list = []
-#         series_list = list(file[col].values)
-#         for value in series_list:
-#             if '*' in str(value):
-#                 value = str(value).replace('*', '')
-#             elif ',' in str(value):
-#                 value = str(value).replace(',', '')
-#             new_list.append(value)
-#         file[col] = pd.Series(new_list)
-#         file[col] = pd.to_numeric(file[col])
-#     file['Enrolled 4-Year (%)'] = (file['Enrolled 4-Year'] / file['Total Graduated']) * 100
-#     file['Year'] = int(file_name[19:23])
-#     wanted_dists = []
-#     for dist_name in list(file['DistName'].values):
-#         if ' ISD' in dist_name:
-#             wanted_dists.append(dist_name)
-#     file = file.loc[file['DistName'].isin(wanted_dists)]
-#     fixed_enrol_dfs.append(file)
-# total_enrollment = pd.concat(fixed_enrol_dfs)
-# test = pd.merge(SAT, total_enrollment, on=['DistName', 'Year'], how='inner')
-# test = test.sort_values(['DistName', 'Year'])
-# test.to_csv('Total_Enrollment.csv', index=False)
+SAT = pd.read_csv('Total_SAT.csv')
+SAT = SAT[['DistName', 'RegnName', 'Year']]
+
+### Fixing College Enrollment DFs ###
+path = 'College_Enrollment/*'
+enrol_files = glob.glob(path)
+fixed_enrol_dfs = []
+for file_name in enrol_files:
+    file = pd.read_csv(file_name)
+    file = file[['DistName', 'Students Enrolled in Texas Public 4-year Universities',
+                 'Total High School Graduates']]
+    file.columns = ['DistName', 'Enrolled 4-Year', 'Total Graduated']
+    col_list = file.columns[1:]
+    dist = list(file['DistName'])
+    dist = [str(i) for i in dist]
+    dist = [i[9:] for i in dist]
+    file['DistName'] = pd.Series(dist)
+    for col in col_list:
+        new_list = []
+        series_list = list(file[col].values)
+        for value in series_list:
+            if '*' in str(value):
+                value = str(value).replace('*', '')
+            elif ',' in str(value):
+                value = str(value).replace(',', '')
+            new_list.append(value)
+        file[col] = pd.Series(new_list)
+        file[col] = pd.to_numeric(file[col])
+    file['Enrolled 4-Year (%)'] = (file['Enrolled 4-Year'] / file['Total Graduated']) * 100
+    file['Year'] = int(file_name[19:23])
+    wanted_dists = []
+    for dist_name in list(file['DistName'].values):
+        if ' ISD' in dist_name:
+            wanted_dists.append(dist_name)
+    file = file.loc[file['DistName'].isin(wanted_dists)]
+    fixed_enrol_dfs.append(file)
+total_enrollment = pd.concat(fixed_enrol_dfs)
+SAT = pd.read_csv('Total_SAT.csv')
+SAT = SAT[['DistName', 'RegnName', 'Year']]
+test = pd.merge(SAT, total_enrollment, on=['DistName', 'Year'], how='inner')
+test = test.sort_values(['DistName', 'Year'])
+test.to_csv('Total_Enrollment.csv', index=False)
 
 ### SAT/ACT ###
 # tests = ['SAT', 'ACT']
-# major_regions = ['Houston', 'San Antonio', 'Austin', 'Richardson', 'Fort Worth']
+major_regions = ['Houston', 'San Antonio', 'Austin', 'Richardson', 'Fort Worth']
 # for test in tests:
 #     fixed_test_dfs = []
 #     path = test + '/*'
@@ -87,32 +92,32 @@ pd.set_option('display.width', 1000)
 #     new_test_df.to_csv('Total_' + test + '.csv', index=False)
 
 ### AP ###
-# fixed_AP_dfs = []
-# path = 'AP/*'
-# AP_files = glob.glob(path)
-# for file_name in AP_files:
-#     file = pd.read_csv(file_name)
-#     cols = file.columns[7:]
-#     for col in cols:
-#         series_list = list(file[col].values)
-#         value_list = []
-#         for value in series_list:
-#             if '<' in str(value):
-#                 value = int(str(value).replace('<', '')) * (1 - .1)
-#             elif ',' in str(value):
-#                 value = int(str(value).replace(',', ''))
-#             value_list.append(value)
-#         file[col] = pd.Series(value_list)
-#         file[col] = pd.to_numeric(file[col])
+fixed_AP_dfs = []
+path = 'AP/*'
+AP_files = glob.glob(path)
+for file_name in AP_files:
+    file = pd.read_csv(file_name)
+    cols = file.columns[7:]
+    for col in cols:
+        series_list = list(file[col].values)
+        value_list = []
+        for value in series_list:
+            if '<' in str(value):
+                value = int(str(value).replace('<', '')) * (1 - .1)
+            elif ',' in str(value):
+                value = int(str(value).replace(',', ''))
+            value_list.append(value)
+        file[col] = pd.Series(value_list)
+        file[col] = pd.to_numeric(file[col])
 #     file = file[['DistName', 'RegnName', 'Exnees_Mskd', 'Exams_Mskd', 'Exams_Above_Crit_Rate']]
-#     file.columns = ['DistName', 'RegnName', 'AP-11&12 Participating Students', 'AP-Total Exams', 'AP-Passed(%)']
-#     file['AP-Exams Taken Per Student'] = file['AP-Total Exams'] / file['AP-11&12 Participating Students']
-#     file['Year'] = int(file_name[3:7])
-#     wanted_dists = []
-#     for dist_name in list(file['DistName'].values):
-#         if ' ISD' in dist_name:
-#             wanted_dists.append(dist_name)
-#     file = file.loc[(file['RegnName'].isin(major_regions)) & (file['DistName'].isin(wanted_dists)) & (file['AP-Total Exams'] > 50)]
+    file.columns = ['DistName', 'RegnName', 'AP-11&12 Participating Students', 'AP-Total Exams', 'AP-Passed(%)']
+    file['AP-Exams Taken Per Student'] = file['AP-Total Exams'] / file['AP-11&12 Participating Students']
+    file['Year'] = int(file_name[3:7])
+    wanted_dists = []
+    for dist_name in list(file['DistName'].values):
+        if ' ISD' in dist_name:
+            wanted_dists.append(dist_name)
+    file = file.loc[(file['RegnName'].isin(major_regions)) & (file['DistName'].isin(wanted_dists)) & (file['AP-Total Exams'] > 50)]
 #     fixed_AP_dfs.append(file)
 # new_AP_df = pd.concat(fixed_AP_dfs).sort_values('Year')
 # new_AP_df = new_AP_df.dropna()
@@ -147,12 +152,18 @@ pd.set_option('display.width', 1000)
 # new_wealth.to_csv('Total_Wealth.csv', index=False)
 
 ### Merging SAT, ACT, AP, Enrollment, Wealth/ADA ###
-# path = 'Total_*'
-# file_names = glob.glob(path)
-# dfs = [pd.read_csv(file) for file in file_names]
-# Total_Merged = reduce(lambda x, y: pd.merge(x, y, on=['DistName', 'RegnName', 'Year'], how='inner'), dfs)
-# Total_Merged = Total_Merged.sort_values(['DistName', 'Year'])
-# Total_Merged.to_csv('Seven_Year_Historical.csv', index=False)
+path = 'Total_*'
+file_names = glob.glob(path)
+dfs = [pd.read_csv(file) for file in file_names]
+Total_Merged = reduce(lambda x, y: pd.merge(x, y, on=['DistName', 'RegnName', 'Year'], how='inner'), dfs)
+Total_Merged = Total_Merged.sort_values(['DistName', 'Year'])
+wanted_dfs = []
+for dist in list(Total_Merged['DistName'].unique()):
+    dist_df = Total_Merged.loc[Total_Merged['DistName'] == dist]
+    if len(dist_df) == 7:
+        wanted_dfs.append(dist_df)
+Total_Merged = pd.concat(wanted_dfs)
+Total_Merged.to_csv('Seven_Year_Historical.csv', index=False)
 
 ### Getting School Districts With the Full Seven Years of Data ###
 # seven = pd.read_csv('Seven_Year_Historical.csv')
